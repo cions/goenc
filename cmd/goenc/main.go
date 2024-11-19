9f2580d230a9c61de597438b043246325afce865
cions <gh.cions@gmail.com>
2024-11-19 19:48:33 +0900

    Migrate option parsing to github.com/cions/go-options

--- cmd/goenc/main.go
+++ cmd/goenc/main.go
@@ -10,18 +10,154 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"runtime"
 	"runtime/debug"
+	"strconv"
+	"strings"
 
+	"github.com/cions/go-options"
 	"github.com/cions/goenc"
 	"github.com/cions/goenc/prompt"
 )
 
-func getVersion() string {
-	if bi, ok := debug.ReadBuildInfo(); ok {
-		return bi.Main.Version
+var NAME = "goenc"
+var VERSION = "(devel)"
+var USAGE = `Usage: $NAME [OPTIONS] [INPUT] [OUTPUT]
+
+A simple file encryption tool
+
+Options:
+  -e, --encrypt         Encrypt
+  -d, --decrypt         Decrypt
+  -n, --no-clobber      Do not overwrite an existing file
+  -t, --time=N          Argon2 time parameter (default: 8)
+  -m, --memory=N[KMG]   Argon2 memory parameter (default: 256M)
+  -p, --parallelism=N   Argon2 parallelism parameter (default: 4)
+  -r, --retries=N       Maximum number of attempts to enter password
+                        (default: 3)
+  -h, --help            Show this help message and exit
+      --version         Show version information and exit
+
+Environment Variables:
+  PASSWORD              Encryption password
+
+Exit Status:
+  0  Operation was successful
+  1  Tag verification failed (password is wrong or data is corrupted)
+  2  Invalid command line
+  3  An error occurred
+`
+
+type Options struct {
+	Encrypt   bool
+	NoClobber bool
+	Time      uint32
+	Memory    uint32
+	Threads   uint8
+	Retries   uint8
+	Input     string
+	Output    string
+}
+
+func (opts *Options) Kind(name string) options.Kind {
+	switch name {
+	case "-e", "--encrypt":
+		return options.Boolean
+	case "-d", "--decrypt":
+		return options.Boolean
+	case "-n", "--no-clobber":
+		return options.Boolean
+	case "-t", "--time":
+		return options.Required
+	case "-m", "--memory":
+		return options.Required
+	case "-p", "--parallelism":
+		return options.Required
+	case "-r", "--retries":
+		return options.Required
+	case "-h", "--help":
+		return options.Boolean
+	case "--version":
+		return options.Boolean
+	default:
+		return options.Unknown
 	}
-	return "(devel)"
+}
+
+func (opts *Options) Option(name string, value string, hasValue bool) error {
+	switch name {
+	case "-e", "--encrypt":
+		opts.Encrypt = true
+	case "-d", "--decrypt":
+		opts.Encrypt = false
+	case "-n", "--no-clobber":
+		opts.NoClobber = true
+	case "-t", "--time":
+		n, err := strconv.ParseUint(value, 10, 32)
+		if err != nil {
+			return err
+		} else if n == 0 {
+			return strconv.ErrRange
+		}
+		opts.Time = uint32(n)
+	case "-m", "--memory":
+		unit := uint64(1)
+		width := 32
+		switch {
+		case strings.HasSuffix(value, "K"):
+			value = strings.TrimSuffix(value, "K")
+		case strings.HasSuffix(value, "M"):
+			value = strings.TrimSuffix(value, "M")
+			unit = 1 << 10
+			width -= 10
+		case strings.HasSuffix(value, "G"):
+			value = strings.TrimSuffix(value, "G")
+			unit = 1 << 20
+			width -= 20
+		}
+
+		n, err := strconv.ParseUint(value, 10, width)
+		if err != nil {
+			return err
+		} else if n == 0 {
+			return strconv.ErrRange
+		}
+		opts.Memory = uint32(n * unit)
+	case "-p", "--parallelism":
+		n, err := strconv.ParseUint(value, 10, 8)
+		if err != nil {
+			return err
+		} else if n == 0 {
+			return strconv.ErrRange
+		}
+		opts.Threads = uint8(n)
+	case "-r", "--retries":
+		n, err := strconv.ParseUint(value, 10, 8)
+		if err != nil {
+			return err
+		} else if n == 0 {
+			return strconv.ErrRange
+		}
+		opts.Retries = uint8(n)
+	case "-h", "--help":
+		return options.ErrHelp
+	case "--version":
+		return options.ErrVersion
+	default:
+		return options.ErrUnknown
+	}
+	return nil
+}
+
+func (opts *Options) Arg(index int, value string, afterDDash bool) error {
+	switch index {
+	case 0:
+		opts.Input = value
+	case 1:
+		opts.Output = value
+	default:
+		return options.Errorf("too many arguments")
+	}
+	return nil
 }
 
 func getPassword(maxRetries uint8) ([]byte, error) {
@@ -48,7 +184,7 @@ func getPassword(maxRetries uint8) ([]byte, error) {
 		if bytes.Equal(password, confirmPassword) {
 			return password, nil
 		} else if tries < maxRetries {
-			fmt.Fprintln(terminal, "goenc: error: passwords does not match. try again.")
+			fmt.Fprintf(terminal, "%v: error: passwords does not match. try again.\n", NAME)
 			tries++
 		} else {
 			return nil, errors.New("passwords does not match")
@@ -56,7 +192,7 @@ func getPassword(maxRetries uint8) ([]byte, error) {
 	}
 }
 
-func encrypt(opts *options) error {
+func encrypt(opts *Options) error {
 	var r io.Reader = os.Stdin
 	if opts.Input != "-" {
 		fh, err := os.Open(opts.Input)
@@ -127,7 +263,7 @@ func tryDecrypt(input []byte, maxRetries uint8) ([]byte, error) {
 		}
 		plaintext, err := goenc.Decrypt(password, input)
 		if errors.Is(err, goenc.ErrInvalidTag) && tries < maxRetries {
-			fmt.Fprintln(terminal, "goenc: error: incorrect password. try again.")
+			fmt.Fprintf(terminal, "%v: error: incorrect password. try again.\n", NAME)
 			tries++
 			continue
 		} else if err != nil {
@@ -137,7 +273,7 @@ func tryDecrypt(input []byte, maxRetries uint8) ([]byte, error) {
 	}
 }
 
-func decrypt(opts *options) error {
+func decrypt(opts *Options) error {
 	var r io.Reader = os.Stdin
 	if opts.Input != "-" {
 		fh, err := os.Open(opts.Input)
@@ -179,9 +315,9 @@ func decrypt(opts *options) error {
 	return nil
 }
 
-func runCommand(args []string) error {
-	opts := &options{
-		Operation: opEncrypt,
+func run(args []string) error {
+	opts := &Options{
+		Encrypt:   true,
 		NoClobber: false,
 		Time:      8,
 		Memory:    256 << 10, // 256 MiB
@@ -191,37 +327,44 @@ func runCommand(args []string) error {
 		Output:    "-",
 	}
 
-	err := opts.ParseArgs(args)
-	if err != nil {
+	_, err := options.Parse(opts, args)
+	if errors.Is(err, options.ErrHelp) {
+		usage := strings.ReplaceAll(USAGE, "$NAME", NAME)
+		fmt.Print(usage)
+		return nil
+	} else if errors.Is(err, options.ErrVersion) {
+		version := VERSION
+		if bi, ok := debug.ReadBuildInfo(); ok {
+			version = bi.Main.Version
+		}
+		fmt.Printf("%v %v\n", NAME, version)
+		return nil
+	} else if err != nil {
 		return err
 	}
 
-	switch opts.Operation {
-	case opHelp:
-		fmt.Println(helpMessage)
-	case opVersion:
-		fmt.Printf("goenc %s (%s/%s)\n", getVersion(), runtime.GOOS, runtime.GOARCH)
-	case opEncrypt:
+	if opts.Encrypt {
 		return encrypt(opts)
-	case opDecrypt:
+	} else {
 		return decrypt(opts)
-	default:
-		panic("goenc: invalid operation")
 	}
-
-	return nil
 }
 
 func main() {
-	if err := runCommand(os.Args[1:]); err != nil {
-		var se prompt.SignalError
-		if errors.As(err, &se) {
-			os.Exit(128 + se.Signal())
+	if err := run(os.Args[1:]); err != nil {
+		var sigerr prompt.SignalError
+		if errors.As(err, &sigerr) {
+			os.Exit(128 + sigerr.Signal())
 		}
-		fmt.Fprintf(os.Stderr, "goenc: error: %v\n", err)
-		if errors.Is(err, goenc.ErrInvalidTag) {
+
+		fmt.Fprintf(os.Stderr, "%v: error: %v\n", NAME, err)
+		switch {
+		case errors.Is(err, goenc.ErrInvalidTag):
 			os.Exit(1)
+		case errors.Is(err, options.ErrCmdline):
+			os.Exit(2)
+		default:
+			os.Exit(3)
 		}
-		os.Exit(2)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/cions/goenc
 go 1.22
 
 require (
-	golang.org/x/crypto v0.26.0
-	golang.org/x/sys v0.24.0
-	golang.org/x/term v0.23.0
-	golang.org/x/text v0.17.0
+	github.com/cions/go-options v0.2.0
+	golang.org/x/crypto v0.29.0
+	golang.org/x/sys v0.27.0
+	golang.org/x/term v0.26.0
+	golang.org/x/text v0.20.0
 )
